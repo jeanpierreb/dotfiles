b32e05ba1c71ce492ca4637c7146f262fb3b55b6
Jean-Pierre Barthelemy <jeanpierreb@users.noreply.github.com>
2015-12-14 16:18:30 -0500

    Update brew.sh to install homebrew and x-code clu

--- brew.sh
+++ brew.sh
@@ -8,6 +8,12 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+# Install X-Code command line utilities
+xcode-select --install
+
+# Install homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 # Make sure we’re using the latest Homebrew.
 brew update
 
