70527665c7ae4824a2b35369c5070cd0e32b3920
Jean-Pierre <jeanpierreb@gmail.com>
2025-10-08 20:14:42 -0400

    Removed typo

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,6 @@
 cd "$(dirname "${BASH_SOURCE}")"
 git pull origin master
 
-cp .gitconfig ~/.config/
-
 function doIt() {
     rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
         --exclude "README.md" --exclude "LICENSE" --exclude "bin/" \
